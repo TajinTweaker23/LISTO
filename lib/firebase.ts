9dc2c4bd80e83110ebfca6845ad7aa2c41198746
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-05-16 15:15:26 -0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBtK9nGuMz3mJkYhuKMCu0pa8LASmo1sFU",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
