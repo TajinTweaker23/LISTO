4c1a2caddeff21a7dcb72cc6f7e3fd0ec4cdb9fd
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-05-27 11:16:03 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allow all external domains
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
