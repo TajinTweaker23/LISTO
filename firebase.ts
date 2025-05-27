fbdddcf95d9dda1a0dc68c8632f624be53b8a887
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-05-27 13:10:17 -0600

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 // firebase.ts
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -12,5 +12,5 @@ const firebaseConfig = {
   measurementId: "G-DBP3FC85KC"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
