064b8ed57ed82b54a765852ac92c6ec8b7462a31
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-05-27 13:14:12 -0600

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -12,5 +12,7 @@ const firebaseConfig = {
   measurementId: "G-DBP3FC85KC",
 };
 
+// ✅ this line prevents the duplicate error:
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
 export const db = getFirestore(app);
