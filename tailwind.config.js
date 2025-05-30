e052108608c1070cd5edadea8fa435df4f2edb86
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-05-30 15:10:44 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,23 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        cream:    "#FFF7E0",
-        blush:    "#FBE4E4",
-        sky:      "#E0ECF7",
-        fog:      "#E3E8F0",
-        apricot:  "#FFF3E7",
-        mint:     "#E6F4EA",
-        cloud:    "#F1E3F3",
-        sage:     "#46675B"
-      }
-    }
+        cream:   "#FFF7E0",
+        blush:   "#FBE4E4",
+        sky:     "#E0ECF7",
+        fog:     "#E3E8F0",
+        apricot: "#FFF3E7",
+        mint:    "#E6F4EA",
+        cloud:   "#F1E3F3",
+        sage:    "#46675B",
+      },
+    },
   },
-  plugins: [ require('@tailwindcss/line-clamp') ],
-}
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+  ],
+};
