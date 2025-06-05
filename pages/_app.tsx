43262378a2dbb6941664da4aa3a2a1f77b6a540d
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-06-05 14:35:40 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 // pages/_app.tsx
-import type { AppProps } from "next/app";
-import { AuthProvider } from "../lib/firebase";
-import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+import { AuthProvider } from "../lib/firebase";
+import type { AppProps } from "next/app";
+import "../styles/globals.css"; // Only if you have a global CSS file
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
   );
 }
+
+export default MyApp;
