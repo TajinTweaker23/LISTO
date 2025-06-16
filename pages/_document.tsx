29da94984522d4369b928921bf3d24e714bf6cf9
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-06-16 11:58:38 -0600

    fix google fonts crossorigin attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -7,7 +6,7 @@ export default function Document() {
       <Head>
         {/* Google Fonts: Inter and Poppins */}
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Poppins:wght@500;700&display=swap" rel="stylesheet"/>
       </Head>
       <body>
