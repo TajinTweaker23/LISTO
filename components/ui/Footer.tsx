344e0a5b33f07ce6aa857b872220e57f5dae79fa
TajinTweaker23 <yahairapacheco14@gmail.com>
2025-06-16 18:39:30 -0600

    Update layout and fix image fill warnings

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+
 export default function Footer() {
-  return <footer className="text-center py-4 opacity-70">© {new Date().getFullYear()} LISTO</footer>;
+  return (
+    <footer className="text-center py-4 opacity-70">
+      © {new Date().getFullYear()} LISTO
+    </footer>
+  );
 }
